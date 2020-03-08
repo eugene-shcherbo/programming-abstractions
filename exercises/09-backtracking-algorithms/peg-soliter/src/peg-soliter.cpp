43dcec1535b07520b3c2c67f310309bad637bc6c
Eugene Shcherbo <eugene.shcherbo@gmail.com>
2020-03-08 16:23:46 +0300

    refactor the solution a bit and optimize so that it finds the solution now

--- exercises/09-backtracking-algorithms/peg-soliter/src/peg-soliter.cpp
+++ exercises/09-backtracking-algorithms/peg-soliter/src/peg-soliter.cpp
@@ -7,7 +7,6 @@
 using namespace std;
 
 const int BOARD_SIZE = 7;
-const int N_CELLS = 33;
 
 enum State {
     EMPTY = 0,
@@ -17,13 +16,13 @@ enum State {
 
 void initBoard(Grid<State>& board);
 bool pegSolitaire(Grid<State>& board);
-bool pegSolitairHelper(Grid<State>& board, Vector<Point> holes);
-bool canJumpToHole(const Grid<State>& board, const Point& point, const Direction& dir);
-Point adjacentPoint(const Point& pt, const Direction& dir);
-Point getBetweenPoint(const Point& pt, const Direction& dir);
-bool hasPegInDirection(const Grid<State>& board, const Point& pt, const Direction& dir);
-void jump(Grid<State>& board, Vector<Point>& holes, const Point& source, const Direction& dir);
-void jumpBack(Grid<State>& board, Vector<Point>& holes, const Point& source, const Direction& dir);
+bool canJump(const Grid<State>& board, Point point, Direction dir);
+bool hasPegInDirection(const Grid<State>& board, Point pt, Direction dir);
+void jump(Grid<State>& board, Point src, Direction dir, State srcVal, State betweenVal, State destVal);
+int pegsNumber(const Grid<State>& board);
+void getHoles(const Grid<State>& board, Vector<Point>& holes);
+Point adjacentPoint(Point pt, Direction dir);
+Point getBetweenPoint(Point pt, Direction dir);
 
 int main() {
     Grid<State> board(BOARD_SIZE, BOARD_SIZE);
@@ -52,27 +51,21 @@ void initBoard(Grid<State>& board) {
 }
 
 bool pegSolitaire(Grid<State>& board) {
-    Vector<Point> holes { Point(3, 3) };
-    return pegSolitairHelper(board, holes);
-}
+    if (pegsNumber(board) == 1 && board[3][3] == PEG) return true;
 
-bool pegSolitairHelper(Grid<State>& board, Vector<Point> holes) {
-    int n = holes.size();
+    Vector<Point> holes;
+    getHoles(board, holes);
 
-    if (n == N_CELLS - 1) return true;
-
-    for (int i = n - 1; i >= 0; i--) {
+    for (int i = holes.size() - 1; i >= 0; i--) {
         for (Direction dir = NORTH; dir <= WEST; dir++) {
-            if (canJumpToHole(board, holes[i], dir)) {
-                Point hole = holes[i];
+            if (canJump(board, holes[i], dir)) {
+                jump(board, holes[i], dir, PEG, HOLE, HOLE);
 
-                jump(board, holes, hole, dir);
-
-                if (pegSolitairHelper(board, holes)) {
+                if (pegSolitaire(board)) {
                     return true;
                 }
 
-                jumpBack(board, holes, hole, dir);
+                jump(board, holes[i], dir, HOLE, PEG, PEG);
             }
         }
     }
@@ -80,7 +73,7 @@ bool pegSolitairHelper(Grid<State>& board, Vector<Point> holes) {
     return false;
 }
 
-bool canJumpToHole(const Grid<State>& board, const Point& source, const Direction& dir) {
+bool canJump(const Grid<State>& board, Point source, Direction dir) {
     Point dest = adjacentPoint(source, dir);
 
     return board.inBounds(dest.getY(), dest.getX())
@@ -88,53 +81,58 @@ bool canJumpToHole(const Grid<State>& board, const Point& source, const Directio
             && hasPegInDirection(board, source, dir);
 }
 
-Point adjacentPoint(const Point& pt, const Direction& dir) {
+bool hasPegInDirection(const Grid<State>& board, Point pt, Direction dir) {
+    Point between = getBetweenPoint(pt, dir);
+    return board[between.getY()][between.getX()] == PEG;
+}
+
+
+void jump(Grid<State>& board, Point source, Direction dir, State srcVal, State betweenVal, State destVal) {
+    Point dest = adjacentPoint(source, dir);
+    Point between = getBetweenPoint(source, dir);
+
+    board[source.getY()][source.getX()] = srcVal;
+    board[between.getY()][between.getX()] = betweenVal;
+    board[dest.getY()][dest.getX()] = destVal;
+}
+
+Point adjacentPoint(Point pt, Direction dir) {
     switch (dir) {
         case NORTH: return Point(pt.getX(), pt.getY() - 2);
         case SOUTH: return Point(pt.getX(), pt.getY() + 2);
         case WEST: return Point(pt.getX() - 2, pt.getY());
         case EAST: return Point(pt.getX() + 2, pt.getY());
-        default: return pt;
     }
+    return pt;
 }
 
-bool hasPegInDirection(const Grid<State>& board, const Point& pt, const Direction& dir) {
-    Point between = getBetweenPoint(pt, dir);
-    return board[between.getY()][between.getX()] == PEG;
-}
-
-void jump(Grid<State>& board, Vector<Point>& holes, const Point& source, const Direction& dir) {
-    Point dest = adjacentPoint(source, dir);
-    Point between = getBetweenPoint(source, dir);
-
-    board[dest.getY()][dest.getX()] = HOLE;
-    board[between.getY()][between.getX()] = HOLE;
-    board[source.getY()][source.getX()] = PEG;
-
-    holes.removeBack();
-    holes.add(between);
-    holes.add(dest);
-}
-
-Point getBetweenPoint(const Point& pt, const Direction& dir) {
+Point getBetweenPoint(Point pt, Direction dir) {
     switch (dir) {
         case NORTH: return Point(pt.getX(), pt.getY() - 1);
         case SOUTH: return Point(pt.getX(), pt.getY() + 1);
         case WEST: return Point(pt.getX() - 1, pt.getY());
         case EAST: return Point(pt.getX() + 1, pt.getY());
-        default: return pt;
     }
+    return pt;
 }
 
-void jumpBack(Grid<State>& board, Vector<Point>& holes, const Point& source, const Direction& dir) {
-    Point dest = adjacentPoint(source, dir);
-    Point between = getBetweenPoint(source, dir);
+int pegsNumber(const Grid<State>& board) {
+    int res = 0;
 
-    board[dest.getY()][dest.getX()] = PEG;
-    board[between.getY()][between.getX()] = PEG;
-    board[source.getY()][source.getX()] = HOLE;
+    for (State el: board) {
+        if (el == PEG)
+            res++;
+    }
 
-    holes.removeBack();
-    holes.removeBack();
-    holes.add(source);
+    return res;
+}
+
+void getHoles(const Grid<State>& board, Vector<Point>& holes) {
+    for (int r = 0; r < board.height(); r++) {
+        for (int c = 0; c < board.width(); c++) {
+            if (board[r][c] == HOLE) {
+                holes.add(Point(c, r));
+            }
+        }
+    }
 }
