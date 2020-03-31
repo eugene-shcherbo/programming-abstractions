b6dd37f095381665d0f8388d3369ec407c563516
Eugene Shcherbo <eugene.shcherbo@gmail.com>
2020-03-31 20:17:39 +0300

    add initial files for anagrams project

---
+++ assignments/anagrams/src/anagramsolver.cpp
@@ -0,0 +1,10 @@
+// This is the CPP file you will edit and turn in. (TODO: Remove this comment!)
+
+#include <string>
+#include "set.h"
+using namespace std;
+
+int findAnagrams(const string& phrase, int max, const Set<string>& dictionary) {
+    // TODO: write this function
+    return 0;   // this is only here so it will compile
+}
